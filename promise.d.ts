7de992d8d8b90255da8024e6ec7a8228893802ef
YJC <yangjiecong@live.com>
2018-01-11 21:33:34 +0800

    modify d.ts

--- promise.d.ts
+++ promise.d.ts
@@ -4,7 +4,9 @@
 
 export { createConnection, createPool } from './types/MysqlPromise';
 
-import { QueryData, QueryResult } from './types/MysqlPromise';
+import { QueryData, QueryResult, Pool, Connection } from './types/MysqlPromise';
 
 export declare type MysqlQueryData = QueryData;
 export declare type MysqlQueryResult = QueryResult;
+export declare type MysqlPool = Pool;
+export declare type MysqlConnection = Connection;
